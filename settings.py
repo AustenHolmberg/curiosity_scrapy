7f92c4d094579c878703868a8772f7671ef54177
Austen Holmberg <aholm2@pdx.edu>
2020-01-06 01:16:54 -0800

    added cli, readme needs update

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_NASA_URL = 'https://mars.jpl.nasa.gov'
 BASE_API_URL = 'https://mars.jpl.nasa.gov/api/v1/raw_image_items/?order=sol+asc%2Cinstrument_sort+desc%2Csample_type_sort+desc%2C+date_taken+asc&per_page=99999&page=0&condition_1=msl%3Amission&condition_2={start_date}%3Adate_taken%3Agte&condition_3={end_date}%3Adate_taken%3Alt&search={filter_str}&extended=thumbnail%3A%3Asample_type%3A%3Anoteq'
-FFMPEG_COMMAND = 'ffmpeg -f concat -safe 0 -i {} -filter:v setpts=5.5*PTS {}'
+FFMPEG_COMMAND = 'ffmpeg -hide_banner -loglevel panic -f concat -safe 0 -i {} -filter:v setpts=5.5*PTS {}'
 CAMS = [
     'FHAZ_RIGHT_A',
     'FHAZ_LEFT_A',
@@ -21,5 +21,13 @@ FRONT_CAMS = CAMS[:4]
 REAR_CAMS = CAMS[4:8]
 LEFT_NAV_CAMS = CAMS[8:10]
 RIGHT_NAV_CAMS = CAMS[10:12]
+
+CAM_MAP = {
+    'front': FRONT_CAMS,
+    'rear': FRONT_CAMS,
+    'lnav': LEFT_NAV_CAMS,
+    'rnav': RIGHT_NAV_CAMS
+}
+
 IMAGES_DIR = os.path.join(os.getcwd(), 'images')
 VIDEOS_DIR = os.path.join(os.getcwd(), 'videos')
